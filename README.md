ad3b22a10c5f22d66e294203e475e974f9bd71db
David Greenhouse <dgreenhouse96@gmail.com>
2022-08-25 17:56:40 -0500

    updated Jacascript and README file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# quiz-trivia
+Music Quiz Game
+
+This project is a trivia based quiz game themed to music. Built with HTML, CSS and JavaScript, it will give you a series of questions to go through while being timed. Youre time through the five questions is your point total.
+
---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+var questions = [{
+
+    question: "Which of the following artists would be at a bar, at nine o'clock, on a saturday",
+    answersArray: [
+        {choice: "Billie Joe Armstrong", correct: false},
+        {choice: "Billy Bob Thornton", correct: false},
+        {choice: "Billy Joel", correct: true},
+        {choice: "Billy Idol", correct: false},
+    ] 
+},
+
+{
+    question: "Which of the following artists was born in the UK",
+    answersArray: [
+        {choice: "Billie Joe Armstrong", correct: false},
+        {choice: "Billy Bob Thornton", correct: false},
+        {choice: "Billy Joel", correct: false},
+        {choice: "Billy Idol", correct: true},
+    ] 
+},
+
+{
+    question: "Which of the following options is the name of the Michael Jackson song",
+    answersArray: [
+        {choice: "Billie Jean", correct: true},
+        {choice: "Billy Bob Thornton", correct: false},
+        {choice: "Billy Joel", correct: false},
+        {choice: "Billie Joe Armstrong", correct: false},
+    ] 
+},
+
+{
+    question: "Which of the following options is not a musician, but a movie star",
+    answersArray: [
+        {choice: "Billie Joe Armstrong", correct: false},
+        {choice: "Billy Bob Thornton", correct: true},
+        {choice: "Billy Joel", correct: false},
+        {choice: "Billy Idol", correct: false},
+    ] 
+},
+
+{
+    question: "Which of the following artists wrote the song, New York State of Mind",
+    answersArray: [
+        {choice: "Billie Joe Armstrong", correct: false},
+        {choice: "Billy Bob Thornton", correct: false},
+        {choice: "Billy Joel", correct: true},
+        {choice: "Billy Idol", correct: false},
+    ] 
+},
+
+]
